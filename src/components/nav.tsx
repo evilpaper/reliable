06dae7c08df99086391641275cef5a7ebacc2e39
Pelle Lundgren <pellburt@gmail.com>
2024-09-13 08:19:47 +0200

    Left align menu and logo on small screens

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -15,32 +15,34 @@ export default function Nav() {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <section className="container z-40 p-4 flex items-center gap-6 w-full">
-      <section className="md:hidden">
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-          <SheetTrigger>Menu</SheetTrigger>
-          <SheetContent
-            side="right"
-            className="w-[300px] sm:w-[400px] bg-black border-none"
-          >
-            <nav className="flex flex-col gap-4">
-              {navItems.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className="text-zinc-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                  onClick={() => setIsOpen(false)}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </nav>
-          </SheetContent>
-        </Sheet>
-      </section>
-      <Link href="/" className="text-xl">
-        Reliable
-      </Link>
+    <section className="container z-40 p-4 flex items-center gap-6 w-full justify-between md:justify-normal">
+      <div className="flex items-center gap-6">
+        <section className="md:hidden">
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger>Menu</SheetTrigger>
+            <SheetContent
+              side="right"
+              className="w-[300px] sm:w-[400px] bg-black border-none"
+            >
+              <nav className="flex flex-col gap-4">
+                {navItems.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="text-zinc-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </section>
+        <Link href="/" className="text-xl">
+          Reliable
+        </Link>
+      </div>
       <section className="flex items-center justify-between md:w-full">
         <nav className="hidden md:flex">
           {navItems.map((item) => (
