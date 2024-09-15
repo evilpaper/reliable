f58abff5232013cba9347e306320b635e3aa2450
Pelle Lundgren <pellburt@gmail.com>
2024-09-15 08:20:14 +0200

    Put cart count in a black absolute dot

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -88,9 +88,13 @@ export function Nav() {
           ))}
         </nav>
         <section className="flex items-center gap-6 ">
-          <Button variant="ghost" size="icon" className="flex gap-2">
+          <Button variant="ghost" size="icon" className="flex gap-2 relative">
             <Icons.shoppingCart />
-            {cartCount !== 0 && cartCount}
+            {cartCount !== 0 && (
+              <div className="absolute bg-black text-white text-xs rounded-full h-6 w-6 flex items-center justify-center top-0 right-0 transform translate-x-1/4 -translate-y-1/4">
+                {cartCount}
+              </div>
+            )}
           </Button>
           <Button>Login</Button>
         </section>
