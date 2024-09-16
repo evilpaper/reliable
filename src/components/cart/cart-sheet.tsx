9ed3f27b16c8edd124b2f20e7223089c4cb575aa
Pelle Lundgren <pellburt@gmail.com>
2024-09-16 21:03:29 +0200

    Render cart content in cart sheet

--- src/components/cart/cart-sheet.tsx
+++ src/components/cart/cart-sheet.tsx
@@ -5,6 +5,7 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { useCart } from "@/features/cart/hooks/use-cart";
 
 interface CartSheetProps {
   isCartOpen: boolean;
@@ -12,12 +13,18 @@ interface CartSheetProps {
 }
 
 export function CartSheet({ isCartOpen, setIsCartOpen }: CartSheetProps) {
+  const { cartContent } = useCart();
   return (
     <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
       <SheetContent side="right">
         <SheetHeader className="self-start text-left mb-6">
           <SheetTitle>Cart</SheetTitle>
-          <SheetDescription></SheetDescription>
+          <SheetDescription>
+            {cartContent.length > 0 &&
+              cartContent.map((cartItem, idx) => {
+                return <p key={`${cartItem}+${idx}`}>{cartItem}</p>;
+              })}
+          </SheetDescription>
         </SheetHeader>
       </SheetContent>
     </Sheet>
