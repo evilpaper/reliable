949a354eeea405a49be5439ada4550cf9b54d12b
Pelle Lundgren <pellburt@gmail.com>
2024-10-01 21:34:29 +0200

    Fetch courses from database for landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,20 +1,21 @@
 import { CourseCard } from "@/components/courses/course-card";
+import { getCourses } from "@/db/queries/course";
 
-const course = {
-  name: "Level 2 Food Hygiene and Safety for Cater",
-  description:
-    "Ensures your staff comply with the requirements of the UK Food Safety Act.",
-  priceInSEK: 299,
-};
+export default async function Page() {
+  const courses = await getCourses();
 
-export default function Page() {
   return (
     <section>
-      <CourseCard
-        name={course.name}
-        description={course.description}
-        priceInSEK={course.priceInSEK}
-      />
+      {courses.map(({ name, courseId, priceInSEK, description }) => {
+        return (
+          <CourseCard
+            key={courseId}
+            name={name}
+            priceInSEK={priceInSEK}
+            description={description}
+          />
+        );
+      })}
     </section>
   );
 }
