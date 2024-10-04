b5b7adcc8a206180dd6e235139ab89b5b3fa0812
Pelle Lundgren <pellburt@gmail.com>
2024-10-04 21:31:26 +0200

    Get course by id, flesh out landing

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,22 +1,21 @@
+import { getCourseById } from "@/db/queries/course";
 import { CourseCard } from "@/components/courses/course-card";
-import { getCourses } from "@/db/queries/course";
 
 export default async function Page() {
-  const courses = await getCourses();
+  const [{ id, courseId, name, priceInSEK, description }] = await getCourseById(
+    1
+  );
 
   return (
-    <section>
-      {courses.map(({ name, courseId, priceInSEK, description }) => {
-        return (
-          <CourseCard
-            key={courseId}
-            courseId={courseId}
-            name={name}
-            priceInSEK={priceInSEK}
-            description={description}
-          />
-        );
-      })}
+    <section className="md:flex gap-6">
+      <h1 className="text-5xl">Get work ready</h1>
+      <CourseCard
+        key={id}
+        courseId={courseId}
+        name={name}
+        priceInSEK={priceInSEK}
+        description={description}
+      />
     </section>
   );
 }
--- src/db/queries/course.ts
+++ src/db/queries/course.ts
@@ -1,6 +1,11 @@
+import { eq } from "drizzle-orm";
 import { db } from "../index";
 import { coursesTable } from "@/db/schema";
 
 export function getCourses() {
   return db.select().from(coursesTable);
+}
+
+export function getCourseById(id: number) {
+  return db.select().from(coursesTable).where(eq(coursesTable.id, id));
 }
