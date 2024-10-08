2ca40bd56ca9e056585e4a624f085934132c3759
Pelle Lundgren <pellburt@gmail.com>
2024-10-08 21:36:28 +0200

    Move landing content, open up for several options

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,21 +1,5 @@
-import { CallToActions } from "@/components/landing/call-to-actions";
-import { Testimonials } from "@/components/landing/testimonials";
+import { OneColumnÓption } from "@/components/landing/one-column-option";
 
 export default async function Page() {
-  return (
-    <section className="container flex flex-col column gap-6">
-      <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl text-center">
-        <strong>Vi fixar din personals livsmedelssäkerhetsdiplom.</strong>
-      </h1>
-      <p className="sm:text-xl md:text-2xl text-center">
-        Webbutbildning – Grundkurs i livsmedelshygien
-      </p>
-      <div className="flex justify-center gap-6">
-        <CallToActions />
-      </div>
-      <div className="flex flex-col md:flex-row gap-6 items-center justify-center">
-        <Testimonials />
-      </div>
-    </section>
-  );
+  return <OneColumnÓption />;
 }
