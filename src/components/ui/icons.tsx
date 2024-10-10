e715483025846d23730d9cbc9879ade61f1f5ceb
Pelle Lundgren <pellburt@gmail.com>
2024-10-10 20:23:19 +0200

    Remove unused imports

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,11 +1,7 @@
 import {
   Loader2,
   ChevronLeft,
-  HandCoins,
   CircleDotDashed,
-  Inbox,
-  Users,
-  Check,
   Edit2,
   Trash2,
   Sun,
@@ -26,11 +22,7 @@ type IconProps = React.HTMLAttributes<SVGElement>;
 export const Icons = {
   chevronLeft: ChevronLeft,
   spinner: Loader2,
-  collected: HandCoins,
   pending: CircleDotDashed,
-  inbox: Inbox,
-  customers: Users,
-  check: Check,
   pen: Edit2,
   trashcan: Trash2,
   sun: Sun,
