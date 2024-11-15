f6854353d9f38caab1f647a933cbf1ab3f4828e3
Pelle Lundgren <pellburt@gmail.com>
2024-11-15 21:22:22 +0100

    Add basic content for purchase success page

--- src/app/(landing)/stripe/purchase-success/page.tsx
+++ src/app/(landing)/stripe/purchase-success/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <p>Success</p>;
+  return <section>
+    <h1>Köp genomfört</h1>
+    <p>Vi har skickat en bekräftelse till</p>
+    <p>user@email.com</p>
+    <br/>
+    <p>Aktiveringskod</p>
+    <p>JGS&0ejd6397A</p>
+  </section>;
 }
