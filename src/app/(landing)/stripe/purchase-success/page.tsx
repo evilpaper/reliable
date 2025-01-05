9bef0dcfc680eb2a2de34cf49af3947742330342
Pelle Lundgren <pellburt@gmail.com>
2025-01-05 18:28:05 +0100

    Check if payment intent succeded

--- src/app/(landing)/stripe/purchase-success/page.tsx
+++ src/app/(landing)/stripe/purchase-success/page.tsx
@@ -1,11 +1,21 @@
+import Stripe from "stripe";
 import { Success } from "@/components/stripe/success";
 
-export default function Page({
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function Page({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const activationId = searchParams.activation_id;
+  const paymentIntentId = searchParams.payment_intent as string;
+
+  const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+
+  if (paymentIntent.status !== "succeeded") {
+    return <div>Payment failed</div>;
+  }
 
   return <Success activationId={activationId as string} />;
 }
