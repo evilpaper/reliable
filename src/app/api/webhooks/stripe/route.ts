d621c06ac1358416df7a2b5734e2d957b9c611df
Pelle Lundgren <pellburt@gmail.com>
2025-01-08 07:56:01 +0100

    Clarify use of constructEvent

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -6,6 +6,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 export async function POST(req: NextRequest) {
   console.log("Yay, Stripe got back to us!");
 
+  /**
+   * This is Stripes suggested approach to secure our webhook.
+   * It add the endpoints signing secret and verify the request so we know it is from Stripe.
+   */
   const event = await stripe.webhooks.constructEvent(
     await req.text(),
     req.headers.get("stripe-signature") as string,
