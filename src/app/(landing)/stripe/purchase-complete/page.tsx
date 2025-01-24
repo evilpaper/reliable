fcb9989a3f4f9fa9eb91fb04951f2d55d63949fb
Pelle Lundgren <pellburt@gmail.com>
2025-01-24 07:55:51 +0100

    Remove activationId

--- src/app/(landing)/stripe/purchase-complete/page.tsx
+++ src/app/(landing)/stripe/purchase-complete/page.tsx
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 import { Success } from "@/components/stripe/success";
-// import { getPurchaseByActivationId } from "@/db/queries/purchase";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
@@ -9,7 +8,6 @@ export default async function Page({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  // const activationId = searchParams.activation_id;
   const paymentIntentId = searchParams.payment_intent as string;
 
   const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
@@ -17,9 +15,6 @@ export default async function Page({
   if (paymentIntent.status !== "succeeded") {
     return <div>Payment failed</div>;
   }
-
-  // const purchase = await getPurchaseByActivationId(activationId as string);
-  // const email = purchase ? purchase.purchaserEmail : "Email not found";
 
   return <Success activationId={"666"} email={"test@test.com"} />;
 }
