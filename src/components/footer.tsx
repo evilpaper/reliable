b626f7a550fb384915d6eee87504b1924343b43d
Pelle Lundgren <pellburt@gmail.com>
2025-01-28 19:27:37 +0100

    Add logo to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,19 @@
 import { ThemeToggle } from "@/components/theme/theme-toggle";
+import Image from "next/image";
 
 const currentYear = new Date().getFullYear();
 
 export function Footer() {
   return (
-    <footer className="container flex justify-between items-center p-4">
-      <p className="text-sm">{`© Reliable ${currentYear}`}</p>
+    <footer className="container flex justify-between items-center p-4 py-12">
+      <p className="text-sm">{`© ${currentYear} The Reliable Club`}</p>
+      <Image
+        src="/reliable-logo.svg"
+        alt="The Reliable Club"
+        width={110}
+        height={38}
+        priority
+      />
       <ThemeToggle />
     </footer>
   );
