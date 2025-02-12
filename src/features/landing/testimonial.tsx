9510668d22d67911ef229f39f2ef552353d9ab80
Pelle Lundgren <pellburt@gmail.com>
2025-02-12 07:12:39 +0100

    Fix testimonial typo

--- src/features/landing/testimonial.tsx
+++ src/features/landing/testimonial.tsx
@@ -5,7 +5,7 @@ import {
 } from "@/common/components/ui/avatar";
 import { Icons } from "@/common/components/ui/icons";
 
-const testinmonial = {
+const testimonial = {
   imagePath: "/mathias-dahlgren.webp",
   initials: "MD",
   name: "Mathias Dalgren",
@@ -20,23 +20,23 @@ export function Testimonial() {
     <section className="max-w-sm mx-4 sm:mx-0 p-8">
       <div className="flex items-center space-x-4 mb-4">
         <Avatar className="w-12 h-12">
-          <AvatarImage src={testinmonial.imagePath} alt="Avatar image" />
-          <AvatarFallback>{testinmonial.initials}</AvatarFallback>
+          <AvatarImage src={testimonial.imagePath} alt="Avatar image" />
+          <AvatarFallback>{testimonial.initials}</AvatarFallback>
         </Avatar>
         <div>
-          <h2 className="font-semibold">{testinmonial.name}</h2>
-          <p className="text-sm text-gray-400">{testinmonial.title}</p>
+          <h2 className="font-semibold">{testimonial.name}</h2>
+          <p className="text-sm text-gray-400">{testimonial.title}</p>
         </div>
       </div>
       <div className="flex mb-4">
-        {Array.from(Array(testinmonial.stars).keys()).map((_, i) => (
+        {Array.from(Array(testimonial.stars).keys()).map((_, i) => (
           <Icons.star
             key={i}
             className="w-4 h-4 fill-yellow-400 text-yellow-400"
           />
         ))}
       </div>
-      <p>{testinmonial.paragrapgh}</p>
+      <p>{testimonial.paragrapgh}</p>
     </section>
   );
 }
