b7d8b4786b423287b04439af2d5809b12f4debb6
Pelle Lundgren <pellburt@gmail.com>
2025-02-16 13:09:41 +0100

    refactor: use slug instead of id

--- src/app/(landing)/courses/[slug]/page.tsx
+++ src/app/(landing)/courses/[slug]/page.tsx
@@ -3,12 +3,12 @@ import { courses } from "@/features/course/courses";
 
 interface PageProps {
   params: {
-    id: string;
+    slug: string;
   };
 }
 
 export default async function Page({ params }: PageProps) {
-  const course = courses.find((course) => course.slug === params.id);
+  const course = courses.find((course) => course.slug === params.slug);
 
   if (!course) {
     return <h1>Hm...the course you tried to reach does not exist.</h1>;
