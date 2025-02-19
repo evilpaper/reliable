ec9ad89d428dc0842cecfe7db7bada3154d0b27d
Pelle Lundgren <pellburt@gmail.com>
2025-02-19 12:31:04 +0100

    style: wrap lesson in container to center on the screen

--- src/app/(landing)/courses/[courseSlug]/lessons/[id]/page.tsx
+++ src/app/(landing)/courses/[courseSlug]/lessons/[id]/page.tsx
@@ -12,10 +12,7 @@ export default async function Page({ params }: PageProps) {
     (course) => course.courseSlug === params.courseSlug
   );
 
-  // console.log(course);
-
   const lesson = course?.lessons.find((lesson) => {
-    console.log(params.id);
     return lesson.lessonNumber.toString() === params.id;
   });
 
@@ -23,5 +20,9 @@ export default async function Page({ params }: PageProps) {
     return <h1>Hm...the lesson you tried to reach does not exist.</h1>;
   }
 
-  return <p>{lesson.content}</p>;
+  return (
+    <section className="container max-w-3xl flex-1 p-4 md:p-0 mt-16">
+      <p>{lesson.content}</p>
+    </section>
+  );
 }
