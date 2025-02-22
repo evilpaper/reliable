bad8f425e3feb6f213bf941ec8c9e940051cf98b
Pelle Lundgren <pellburt@gmail.com>
2025-02-22 13:27:21 +0100

    feat(lesson): show answer to question

--- src/features/course/quiz.tsx
+++ src/features/course/quiz.tsx
@@ -2,7 +2,12 @@
 
 import React from "react";
 import { Button } from "@/common/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/common/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/common/components/ui/card";
 
 export function Quiz(lesson: {
   question?: string;
@@ -21,11 +26,11 @@ export function Quiz(lesson: {
     }
   };
   return (
-    <Card className="bg-gray-50/50 border-0 rounded-2xl h-full">
+    <Card className="bg-gray-50/50 border-0 rounded-2xl h-full w-full">
       <CardHeader>
         <span className="text-gray-600 font-medium">Quiz</span>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-6">
         {lesson.options?.map((option, index) => (
           <Button
             key={index}
@@ -38,6 +43,23 @@ export function Quiz(lesson: {
           </Button>
         ))}
       </CardContent>
+      {isAnswered && (
+        <CardFooter>
+          <div className="flex w-full rounded-md border border-stone-400 p-4">
+            {selectedAnswer === lesson.correctAnswer ? (
+              <p>
+                Correct! <br /> {lesson.explanation}
+              </p>
+            ) : (
+              <p>
+                Incorrect! <br /> The correct answer is: {lesson.correctAnswer}{" "}
+                <br />
+                {lesson.explanation}
+              </p>
+            )}
+          </div>
+        </CardFooter>
+      )}
     </Card>
   );
 }
